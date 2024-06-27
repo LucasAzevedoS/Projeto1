0241fb02cc1c3386e8d483b35a661f757789f72a
Lucas Azevedo de Souza <lucas.a.souza@hotmail.com>
2024-06-26 21:06:16 -0300

    Criando uma nova view

---
+++ Projeto1/Controllers/TesteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto1.Controllers
+{
+
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
